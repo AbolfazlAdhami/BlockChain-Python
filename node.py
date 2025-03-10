3cdf764b416e18dab775ce18383e65a7d3beedcb
AbolfazlAdhami <aadhami880@gmail.com>
2025-03-10 08:05:13 +0330

    fix: node class

--- node.py
+++ node.py
@@ -27,73 +27,64 @@ class Node:
         user_input = input('Your Choice: ')
         return user_input
 
-    def print_block_chain_elements(self):
-        """ Outputting the node and waits for user input. """
+    def print_blockchain_elements(self):
+        """ Output all blocks of the blockchain. """
+        # Output the blockchain list to the console
         for block in self.blockchain.chain:
-            print("Outputting Block")
+            print('Outputting Block')
             print(block)
         else:
-            print('-'*20)
+            print('-' * 20)
 
     def listen_for_input(self):
-        """Start the node and wait for user input"""
+        """Starts the node and waits for user input."""
         waiting_for_input = True
 
-        # A while loop for the user input inferface
-        # It's a loop that exits once waiting_for_input ot when break is called
+        # A while loop for the user input interface
+        # It's a loop that exits once waiting_for_input becomes False or when break is called
         while waiting_for_input:
             print('Please choose')
             print('1: Add a new transaction value')
             print('2: Mine a new block')
             print('3: Output the blockchain blocks')
-            print('4: Output participants')
-            print('5: Check transaction validity')
-            print('h: Manipulate the chain')
+            print('4: Check transaction validity')
             print('q: Quit')
             user_choice = self.get_user_choice()
             if user_choice == '1':
                 tx_data = self.get_transaction_value()
                 recipient, amount = tx_data
                 # Add the transaction amount to the blockchain
-                if self.blockchain.add_transaction(recipient, amount=amount):
+                if self.blockchain.add_transaction(recipient, self.id, amount=amount):
                     print('Added transaction!')
                 else:
                     print('Transaction failed!')
-                print(open_transactions)
+                print(self.blockchain.get_open_transactions())
             elif user_choice == '2':
-                if mine_block():
-                    open_transactions = []
-                save_file(blockchain, open_transactions)
+                self.blockchain.mine_block()
             elif user_choice == '3':
-                print_blockchain_elements()
+                self.print_blockchain_elements()
             elif user_choice == '4':
-                print(participants)
-            elif user_choice == '5':
-                if verify_transactions():
+                if Verification.verify_transactions(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
                     print('All transactions are valid')
                 else:
                     print('There are invalid transactions')
-            elif user_choice == 'h':
-                # Make sure that you don't try to "hack" the blockchain if it's empty
-                if len(blockchain) >= 1:
-                    blockchain[0] = {
-                        'previous_hash': '',
-                        'index': 0,
-                        'transactions': [{'sender': 'Chris', 'recipient': 'Abolfazl', 'amount': 100.0}]
-                    }
             elif user_choice == 'q':
                 # This will lead to the loop to exist because it's running condition becomes False
                 waiting_for_input = False
             else:
                 print('Input was invalid, please pick a value from the list!')
-            if not verify_chain():
-                print_blockchain_elements()
+            if not Verification.verify_chain(self.blockchain.chain):
+                self.print_blockchain_elements()
                 print('Invalid blockchain!')
                 # Break out of the loop
                 break
             print('Balance of {}: {:6.2f}'.format(
-                'Abolfazl', get_balance('Abolfazl')))
+                self.id, self.blockchain.get_balance()))
         else:
             print('User left!')
 
         print('Done!')
+
+
+node = Node()
+node.listen_for_input()
