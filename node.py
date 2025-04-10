c1f5af0fdd2a6c8973c6a093917a36e5c3095b47
AbolfazlAdhami <aadhami880@gmail.com>
2025-04-10 17:58:44 +0330

    feat: save and load keys

--- node.py
+++ node.py
@@ -13,6 +13,41 @@ def get_ui():
     return 'This Works'
 
 
+@app.route('/wallet', methods=['POST'])
+def create_keys():
+    wallet.create_keys()
+    if wallet.save_keys():
+        response = {
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key
+        }
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': "Saving the keys failed."
+        }
+        return jsonify(response), 500
+
+
+@app.route('/wallet', methods=['GET'])
+def load_keys():
+    if wallet.load_keys():
+        response = {
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key
+        }
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        return jsonify(response), 200
+    else:
+        response = {
+            'message': "Saving the keys failed."
+        }
+        return jsonify(response), 500
+
+
 @app.route('/chain', methods=['GET'])
 def get_chain():
     chain_snapshot = blockchain.chain
@@ -26,12 +61,10 @@ def get_chain():
 @app.route('/mine', methods=['POST'])
 def mine():
     block = blockchain.mine_block()
-
     if block != None:
         dict_block = block.__dict__.copy()
         dict_block['transactions'] = [
             tx.__dict__ for tx in dict_block['transactions']]
-        print(dict_block)
         response = {
             'message': 'Block added successfully.',
             'block': dict_block
