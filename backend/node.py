31ef35c839524b1bf175cb88d1e6ee75460f37b2
AbolfazlAdhami <aadhami880@gmail.com>
2025-06-12 00:34:48 +0330

    feat: render on multiple nodes

--- backend/node.py
+++ backend/node.py
@@ -4,8 +4,6 @@ from wallet import Wallet
 from flask_cors import CORS
 from blockchain import Blockchain
 app = Flask(__name__)
-wallet = Wallet()
-blockchain = Blockchain(wallet.public_key)
 CORS(app)
 
 
@@ -218,4 +216,12 @@ def get_nodes():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    from argparse import ArgumentParser
+    parser=ArgumentParser()
+    parser.add_argument('-p','--port',type=int,default=8000)
+    args=parser.parse_args()
+    print(args.port)
+    port=args.port
+    wallet = Wallet(port)
+    blockchain = Blockchain(wallet.public_key,port)
+    app.run(host='0.0.0.0', port=port)
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc. */}
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
